e5aecd01deaf391710d1727987c5719e0008120b
gatapon <git@gatapon.net>
2018-12-17 17:16:29 +0900

    index.php 変更

--- data/html/index.php
+++ data/html/index.php
@@ -9,9 +9,34 @@
     <div class="container">
         <h1 class="title">nginx+PHP+FPM server</h1>
         <img src="/img.jpg" alt="" class="thumbnail">
+        <?php
+         $mysql = mysqli($_ENV['DATABASE_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
+
+         if (!$mysql) {
+             echo "Error: Unable to connect to Mysql" .PHP_EOL;
+             echo "Debugging error: " . mysqli_connect_error() .PHP_EOL;
+             exit;
+         }
+
+         $sql = "INSERT INTO hoges(created_at) VALUES('" . date('Y-m-d H:i:s') . "')";
+
+         $result = $mysql->query($sql);
+
+         $sql = "SELECT * FROM hoges ORDER BY hoge_id desc limit 1";
+
+         $result = $mysql->query($sql)->fetch_row();
+
+         echo '<pre class="log">';
+         var_dump($result);
+         echo '</pre>';
+
+         mysqli_close($mysql);
+
+         phpinfo();
+
+         ?>
     </div>
     <script src="/main.js"></script>
 </body>
 </html>
 <?php
-phpinfo();
