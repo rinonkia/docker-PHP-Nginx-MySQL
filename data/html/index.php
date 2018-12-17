f0753b2b6dc4e7990d488af858d297a2c9c8e5b3
gatapon <git@gatapon.net>
2018-12-17 22:03:02 +0900

    index.php 変更

--- data/html/index.php
+++ data/html/index.php
@@ -2,13 +2,13 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Step.2 nginx+PHP+FPM server</title>
+    <title>Step.4 Enable HTTP/2</title>
     <link rel="stylesheet" href="/main.css">
 </head>
 <body>
     <div class="container">
-        <h1 class="title">nginx+PHP+FPM server</h1>
-        <img src="/img.jpg" alt="" class="thumbnail">
+        <h1 class="title">Enable HTTP/2</h1>
+        <img src="/img.jpg" alt="" class="thumbnail" />
         <?php
         $mysql = new mysqli($_ENV['DATABASE_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
 
